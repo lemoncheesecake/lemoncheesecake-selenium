8ee6f60eab5ac4876fc3cb87fae34c098692ab8d
Nicolas Delon <nicolas.delon@gmail.com>
2021-09-18 14:28:14 +0200

    __init__.py: declare a __all__ for pydoc and sphinx

--- lemoncheesecake_selenium/__init__.py
+++ lemoncheesecake_selenium/__init__.py
@@ -2,3 +2,6 @@ from .selector import Selector
 from .selection import Selection
 from .matchers import has_text, has_attribute, is_displayed, is_enabled, is_selected
 from .utils import save_screenshot
+
+# for pydoc & sphinx
+__all__ = [sym_name for sym_name in dir() if not sym_name.startswith("_")]
